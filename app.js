f32682cd8a5cfd72cfb80738b3278636d05cce70
Dom <122333281+DomenicGTH@users.noreply.github.com>
2023-02-19 01:12:28 +0000

    added remove button

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
 let container = document.querySelector('.container');
 let small = document.querySelector('#small');
 let large = document.querySelector('#large');
-let square = document.querySelectorAll('.square');
-
+let remove = document.querySelector('#remove');
+let block = document.querySelector('.container');
+let deleter = document.querySelector('#remove')
 
 small.addEventListener('click', () => {
-
-        SquaresSmall();
-        
-    });
-
-Array.from(square).forEach(squares => {
-    squares.addEventListener('mouseenter' , () => {
-
-        squares.style.backgroundColor = 'black';
-        squares.style.color = 'black';
-        squares.style.borderColor = 'white';
+    SquaresSmall();
 });
+
+large.addEventListener('click', () => {
+    SquaresLarge();
+});
+
+deleter.addEventListener('click', () => {
+    removeElements();
+});
+
+
+// this sucks really bad down below
+// deleter.addEventListener('click', (e) => {
+//     let test = document.getElementsByClassName('square')
+//     test.forEach(e => {
+//         e.remove()});
     
+//     });
+    // e.forEach(e => e.remove());
+
+
+
+
+block.addEventListener('mouseover' , () => {
+    colorIn();
+
+
 });
 
 
@@ -30,7 +46,30 @@ function SquaresSmall() {
         
         htmlToAdd += `<div class="square"></div>`;
         container.innerHTML = htmlToAdd;
-        
     };
 
+}
+
+function SquaresLarge() {
+    let htmlToAdd = "";
+    let numOfSquares = 64;
+  
+    for (let i = 0; i < numOfSquares; i++) {
+        
+        htmlToAdd += `<div class="square"></div>`;
+        container.innerHTML = htmlToAdd;
+    };
+
+}
+
+function removeElements () {
+    let remove = document.querySelectorAll('.square')
+    remove.forEach(element => {
+       element.remove();
+    });
+   }
+
+function colorIn () {
+    block.style.backgroundColor = 'black';
+    block.style.color = "black";
 }
